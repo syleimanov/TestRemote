e497109af4c453d4de803ceded225b4526f41437
Andrey <glinkin.a.o@yandex.ru>
2021-01-08 21:57:16 +1000

    PLAYEER Andrey

--- NewYearApp/ViewController.swift
+++ NewYearApp/ViewController.swift
@@ -6,14 +6,38 @@
 //
 
 import UIKit
+import AVFoundation
+import AVKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
+    @IBOutlet weak var videoView: UIView!
+    
+    
+        override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupView()
     }
 
-
+    private func setupView() {
+        let path = URL(fileURLWithPath: Bundle.main.path(forResource: "NYVideo", ofType: "mov")!)
+        let player = AVPlayer(url: path)
+        let newLayer = AVPlayerLayer(player: player)
+        newLayer.frame = self.videoView.frame
+        self.videoView.layer.addSublayer(newLayer)
+        newLayer.videoGravity = AVLayerVideoGravity.resizeAspectFill
+        
+        player.play()
+        player.actionAtItemEnd = AVPlayer.ActionAtItemEnd.none
+        
+        NotificationCenter.default.addObserver(self,
+                selector: #selector(ViewController.videoDidPlayToAnd(_:)),
+                name: NSNotification.Name(rawValue: "AVPlayerItemDidPlayToEndTimeNotification"),
+                object: player.currentItem)
+    }
+    @objc func videoDidPlayToAnd(_ notification: Notification) {
+        let player: AVPlayerItem = notification.object as! AVPlayerItem
+        player.seek(to: CMTime.zero)
+    }
 }
 
